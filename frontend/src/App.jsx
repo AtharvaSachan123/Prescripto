226a63ddba3ab51f06b4885944ef9aceeaef515a
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-10-09 02:05:56 +0530

    Routes configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Contact from './pages/Contact'
+import Doctors from './pages/Doctors'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+
+const App = () => {
   return (
-    <h1 className="text-green-700 text-4xl">
-      Hello world!
-    </h1>
+
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointment' element={<MyAppointments />} />
+        
+      </Routes>
+
+    </div>
   )
 }
+
+export default App
