9d0b545710cae94b980bc4b7ebb5c744eb8c0053
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-11-23 18:56:03 +0530

    Speciality section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
 
     return (
         <div className='relative'>
-            <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-gray-300'>
+            <div className='flex  items-center justify-between text-sm py-4 mb-5 border-b border-gray-300'>
                 <img className='w-44 cursor-pointer' src={assets.logo} alt='logo' />
                 <ul className='hidden md:flex items-start gap-5 font-medium'>
                     <NavLink to="/" >
