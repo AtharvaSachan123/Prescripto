31d39aefa9ff428d70ff99e3a2d55eb0317de726
AtharvaSachan123 <atharvasachan823@gmail.com>
2025-02-09 15:55:44 +0530

    My profile page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -18,10 +18,14 @@ const Login = () => {
       <div className=' flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg  '>
         <p className='text-2xl font-semibold'>{state=="Sign Up"?"Create Account":"Login"}</p>
         <p>Please {state=="Sign Up"?"Create Account":"Login"} to book appointment. </p>
-        <div className='w-full mt-4'>
+        {
+          state=="Sign Up"&&
+          <div className='w-full mt-4'>
           <p>Full Name:</p>
           <input type='text' className='border border-zinc-300 rounded w-full p-2 mt-1' value={name} onChange={(e)=>setName(e.target.value)} required />
-          
+          </div>
+        }
+        
           <div className='w-full mt-2'>
           <p>Email:</p>
           <input type='text' className='border border-zinc-300 rounded w-full p-2 mt-1'  value={email} onChange={(e)=>setEmail(e.target.value)} required />
@@ -32,7 +36,7 @@ const Login = () => {
           <input type='text' className='border border-zinc-300 rounded w-full p-2 mt-1' value={password} onChange={(e)=>setPassword(e.target.value)} required />
           </div>
           
-        </div> 
+         
         <button className='bg-primary text-white w-full py-2 rounded-md  text-base mt-4 mb-5 ' onClick={onSubmitHandler}>{state=="Sign Up"?"Create Account":"Login"}</button>
         {
           state=="Sign Up"?
