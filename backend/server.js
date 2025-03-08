5a411f28f9fe332004acb0eefd940c27e7c92eed
AtharvaSachan123 <atharvasachan823@gmail.com>
2025-03-08 17:20:05 +0530

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './config/mongodb.js';
 
 
 //app config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
+
 
 //middleware
 app.use(express.json());
@@ -14,9 +17,11 @@ app.use(cors());
 //api endpoints
 
 app.get('/', (req, res) => {
-    res.send('CHUUUUTTTTT');
+    res.send('System');
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    
+
 });
